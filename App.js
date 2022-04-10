1014daaff80b50e1577620ff83ba8c3eee0e3fbd
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-10 14:05:24 +0100

    Added ESLint.

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform } from 'react-native';
+import React from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-      <>
-          <SafeAreaView style={styles.container}>
-              <Text style={styles.searchBar}>Search</Text>
-              <Text style={styles.content}>Our React Native blank canvas</Text>
-          </SafeAreaView>
-          <ExpoStatusBar style="auto"/>
-      </>
+    <>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.searchBar}>Search</Text>
+        <Text style={styles.content}>Our React Native blank canvas</Text>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight // Returns 'null' on iOS, # on Android
-    },
-    searchBar: {
-        backgroundColor: 'green',
-        padding: 12
-    },
-    content: {
-        flex: 1,
-        backgroundColor: 'blue',
-        padding: 12
-    }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight, // Returns 'null' on iOS, # on Android
+  },
+  searchBar: {
+    backgroundColor: 'green',
+    padding: 12,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 12,
+  },
 });
