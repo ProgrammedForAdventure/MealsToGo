73aeb9af94333d81fd58a61638b90a36766ba7f0
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-10 14:19:58 +0100

    Added searchbar.

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState();
+  const onChangeText = (text) => {
+    console.log(text);
+    setSearchQuery(text);
+  };
+
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.searchBar}>Search</Text>
+        <Searchbar
+          style={styles.searchBar}
+          onChangeText={onChangeText}
+          placeholder="Search"
+          value={searchQuery}
+        />
         <Text style={styles.content}>Our React Native blank canvas</Text>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
