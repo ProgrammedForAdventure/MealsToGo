2b5a53ee5a0a52d47133b310a1b570d10187050b
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-13 19:42:40 +0100

    Added very basic tab navigation.

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { RestaurantsScreen } from './src/screens/restaurants.screen';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './src/infrastructure/theme/index';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
+import { theme } from './src/infrastructure/theme/index';
+import { RestaurantsScreen } from './src/screens/restaurants.screen';
+import { SafeArea } from './src/components/utility/safe-area.component';
+
+const Tab = createBottomTabNavigator();
+
+// const MyTabs () => {} // Private function
+// function MyTabs() { // Main function
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -26,7 +48,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
