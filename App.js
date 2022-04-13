b3c45b4fc3775d9a5c64953ca7a0be413539ee0f
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-13 19:58:48 +0100

    Added icons to navigation.

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import {
   useFonts as useOswald,
@@ -45,12 +46,31 @@ export default function App() {
     return null;
   }
 
+  const TAB_ICON = {
+    Home: 'restaurant',
+    Map: 'map',
+    Settings: 'ios-list-sharp',
+  };
+
+  const createScreenOptions = ({ route }) => {
+    const iconName = TAB_ICON[route.name];
+    console.log(iconName);
+
+    return {
+      tabBarIcon: ({ size, color }) => (
+        <Ionicons name={iconName} size={size} color={color} />
+      ),
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    };
+  };
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Navigator screenOptions={createScreenOptions}>
+            <Tab.Screen name="Home" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
