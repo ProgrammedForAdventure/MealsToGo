5458e045dbf38c715d3d56507a16eb7305314ebd
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-13 22:19:11 +0100

    Successfully showing some mock data.

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -19,22 +19,22 @@ import {
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = 'Some restaurant',
+    name,
     icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
     ],
     address = '100 some random street',
-    isOpenNow = true,
+    isOpenNow,
     rating = 4,
-    isClosedTemporarily = true,
+    isClosedTemporarily,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.round(rating)));
 
   return (
     <RestaurantCard elevation={5}>
-      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <RestaurantCardCover source={{ uri: photos[0] }} />
       <Info>
         <Text variant="label">{name}</Text>
         <Section>
--- src/screens/restaurants.screen.js
+++ src/screens/restaurants.screen.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Searchbar } from 'react-native-paper';
 import { FlatList } from 'react-native';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 import styled from 'styled-components';
 import { SafeArea } from '../components/utility/safe-area.component';
+import { RestaurantsContext } from '../services/restaurants/restaurants.context';
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -20,6 +21,8 @@ export const RestaurantsScreen = () => {
     setSearchQuery(text);
   };
 
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+
   return (
     <SafeArea>
       <SearchContainer>
@@ -31,21 +34,10 @@ export const RestaurantsScreen = () => {
       </SearchContainer>
 
       <RestaurantList
-        data={[
-          { name: 1 },
-          { name: 2 },
-          { name: 3 },
-          { name: 4 },
-          { name: 5 },
-          { name: 6 },
-          { name: 7 },
-          { name: 8 },
-          { name: 9 },
-          { name: 10 },
-          { name: 11 },
-          { name: 12 },
-        ]}
-        renderItem={() => <RestaurantInfoCard />}
+        data={restaurants}
+        renderItem={({ item }) => {
+          return <RestaurantInfoCard restaurant={item} />;
+        }}
         keyExtractor={(item) => item.name}
         contentContainerStyle={{ padding: 16 }}
       />
