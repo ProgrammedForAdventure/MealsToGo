37f66ab5be1bd973d7daf09dee4a03194a7ce693
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-13 17:47:33 +0100

    Added 'closed temporarily' and icon to business info card.

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { View } from 'react-native';
-import { Text, Button, Card, Paragraph } from 'react-native-paper';
+import { View, Image } from 'react-native';
+import { Text, Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
 
 import star from '../../assets/star';
@@ -52,7 +52,7 @@ const SectionEnd = styled(View)`
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Some restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
     ],
@@ -75,7 +75,23 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
               <SvgXml xml={star} width={20} height={20} />
             ))}
             <SectionEnd>
-              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: 'red' }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              {isOpenNow && (
+                <SvgXml
+                  xml={open}
+                  width={20}
+                  height={20}
+                  style={{ marginLeft: 15 }}
+                />
+              )}
+              <Image
+                style={{ marginLeft: 15, width: 15, height: 15 }}
+                source={{ uri: icon }}
+              />
             </SectionEnd>
           </Rating>
         </Section>
