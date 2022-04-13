65ef7e428424bd81acc6d83b37c69304f24ccebe
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-13 19:12:52 +0100

    Added FlatList.

--- src/screens/restaurants.screen.js
+++ src/screens/restaurants.screen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Searchbar } from 'react-native-paper';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, FlatList } from 'react-native';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 import styled from 'styled-components';
 
@@ -13,11 +13,9 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 16 },
+})``;
 
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = useState();
@@ -36,9 +34,25 @@ export const RestaurantsScreen = () => {
         />
       </SearchContainer>
 
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+      <RestaurantList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+        ]}
+        renderItem={() => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
