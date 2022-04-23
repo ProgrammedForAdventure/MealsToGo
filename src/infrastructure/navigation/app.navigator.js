1f3c6395ad1ebf81c2be3aa05894abc76a59b202
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-23 17:56:40 +0100

    Added maps!

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -6,6 +6,7 @@ import { Text } from 'react-native';
 
 import { SafeArea } from '../../components/utility/safe-area.component';
 import { RestaurantsNavigator } from './restaurants.navigator';
+import { MapScreen } from '../../services/map/screens/map.screen';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,12 +16,6 @@ const Tab = createBottomTabNavigator();
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
-  </SafeArea>
-);
-
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
   </SafeArea>
 );
 
@@ -47,7 +42,7 @@ export const AppNavigator = () => {
     <NavigationContainer>
       <Tab.Navigator screenOptions={createScreenOptions}>
         <Tab.Screen name="Home" component={RestaurantsNavigator} />
-        <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
