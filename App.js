93dd24198c09a02ad4eca12a27cfdf44b8b53004
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-28 20:11:09 +0100

    Added Firebase initialization.

--- App.js
+++ App.js
@@ -4,13 +4,22 @@ import { ThemeProvider } from 'styled-components';
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavoritesContextProvider } from './src/services/favorites/favorites.context';
+import { initializeApp } from 'firebase/app';
 
 import { theme } from './src/infrastructure/theme/index';
 import { Navigation } from './src/infrastructure/navigation/index';
 
+// Initialize Firebase
 const firebaseConfig = {
-  
+  apiKey: 'AIzaSyBc2otVhLD68-Ku1H4p07UGT1pAb0wto4M',
+  authDomain: 'meals-to-go-9e65b.firebaseapp.com',
+  projectId: 'meals-to-go-9e65b',
+  storageBucket: 'meals-to-go-9e65b.appspot.com',
+  messagingSenderId: '353855785535',
+  appId: '1:353855785535:web:4298e0d1917ca4e351c68a',
 };
+
+initializeApp(firebaseConfig);
 
 import {
   useFonts as useOswald,
