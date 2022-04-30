ca49097d0a3211260e9f39e22d8781e185bc1d7d
Isaac Elmore <isaac.elmore@gmail.com>
2022-04-30 21:38:30 +0100

    Added login/register buttons.

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import { AccountBackground } from '../components/account.styles';
+import styled from 'styled-components';
 
-export const AccountScreen = () => {
-  return <AccountBackground />;
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+} from '../components/account.styles';
+
+const RegisterButton = styled(AuthButton)`
+  margin-top: ${(props) => props.theme.space[3]};
+`;
+
+export const AccountScreen = ({ navigation }) => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthButton onPress={() => navigation.navigate('Login')}>
+          Login
+        </AuthButton>
+        <RegisterButton onPress={() => navigation.navigate('Register')}>
+          Register
+        </RegisterButton>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
