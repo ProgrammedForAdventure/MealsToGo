63639b224b4a10d051a89381a485fa9f4ed4adcf
Isaac Elmore <isaac.elmore@gmail.com>
2022-05-04 16:20:07 +0100

    Installed Lottie.

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -3,5 +3,6 @@ import { SafeAreaView, StatusBar } from 'react-native';
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 //   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
